1235dcdc8ce86df75b7f43e99d7bc9fb176f0271
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2022-10-24 22:35:26 +0200

    Add github actions and tests structure

---
+++ tests/test_blog.py
@@ -0,0 +1,39 @@
+from blog import app
+
+
+def test_homepage():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+
+
+def test_login():
+    pass
+
+
+def test_logout():
+    pass
+
+
+def test_create_entry():
+    pass
+
+
+def test_create_draft():
+    pass
+
+
+def test_edit_entry():
+    pass
+
+
+def test_delete_entry():
+    pass
+
+
+def test_list_drafts():
+    pass
+
+
+def test_about():
+    pass
